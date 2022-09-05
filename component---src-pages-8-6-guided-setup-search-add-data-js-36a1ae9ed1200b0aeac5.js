b7d8969d0c2d2e98d292abb7251261d854b9c797
Cindy Chang <cindyisachang@gmail.com>
2022-09-05 16:36:14 +0200

    Updates

---
+++ component---src-pages-8-6-guided-setup-search-add-data-js-36a1ae9ed1200b0aeac5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkplatform_onboarding=self.webpackChunkplatform_onboarding||[]).push([[5413],{97037:function(e,t,r){r.r(t);r(67294);var a=r(48437),s=r(23431),d=[{text:"Search",href:"#"},{text:"Guided Setup",href:"../../"},{text:"Step 1: Add data"}],p={"step-1":"","step-2":"","step-3":"","step-4":""};t.default=function(){return(0,s.tZ)(a.Z,{title:"Add data",breadcrumbs:d,section:"Search",stepNumber:1,currentProgress:0,completedSteps:p,hasSidebar:!0})}}}]);
+//# sourceMappingURL=component---src-pages-8-6-guided-setup-search-add-data-js-36a1ae9ed1200b0aeac5.js.map
